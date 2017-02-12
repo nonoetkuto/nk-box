f754c90869715f973a378115e90b44e86cdd6616
nonoetkuto <nonoetkuto@gmail.com>
2017-02-12 05:01:00 +0100

    Add files via upload
    Ajout de boutons

---
+++ nk-chie-hikku.js
@@ -0,0 +1,54 @@
+// fucking constants
+var HIKKU = 104;
+var CHIE = 99;
+
+var a = document.createElement('audio');
+
+$.fn.rotateMeSenpai = function(angle) {
+  return this.each(function() {
+    var $e = $(this); // <- fuck javascript
+
+    $({deg: 0}).animate({deg: angle}, {
+      step: function(t) {
+        $e.css({
+           transform: 'rotate(' + t + 'deg)' // lol
+         });
+      }
+    });
+  });
+};
+
+function chie(){
+	var a = document.createElement('audio');
+	a.setAttribute('src', 'chie.mp3');
+	a.play();
+	$('.nk-logo').rotateMeSenpai(-360);
+}
+
+function hikku(){
+	var a = document.createElement('audio');
+	a.setAttribute('src', 'hikku.mp3');
+	a.play();
+	$('.nk-logo').rotateMeSenpai(360);
+}
+
+$(document).keypress(function(event) {
+	var angle = 0;
+	switch(event.which) {
+		case CHIE: 
+		a.setAttribute('src', 'chie.mp3');
+		a.play();
+		angle = -360;
+		break;
+
+		case HIKKU:
+		a.setAttribute('src', 'hikku.mp3');
+		a.currentTime = 0.2; // a little fucking offset bitch
+		a.play();
+		angle = 360;
+		break;
+	}
+
+	// NOW DO A BARREL ROLL
+	$('.nk-logo').rotateMeSenpai(angle);
+});
