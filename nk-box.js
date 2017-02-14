ff09b04480432fc006a5bd88fe4c737f2041f52e
nonoetkuto <nonoetkuto@gmail.com>
2017-02-14 20:21:05 +0100

    Add files via upload

--- nk-box.js
+++ nk-box.js
@@ -2,6 +2,8 @@
 var HIKKU = 104;
 var CHIE = 99;
 var a = document.createElement('audio');
+var counter = 0;
+var quete1 = "";
 
 $.fn.rotateMeSenpai = function(angle) {
   return this.each(function() {
@@ -17,6 +19,10 @@ $.fn.rotateMeSenpai = function(angle) {
   });
 };
 
+function hasard(min,max){
+	return min+Math.floor(Math.random()*(max-min+1));
+}
+
 function playSound(name, offset) {
 	a.setAttribute('src', 'res/sounds/'+name+'.mp3');
 	if(offset != undefined) 
@@ -30,6 +36,46 @@ function randomson() {
 	var len = $('.btnSound').length;
 	var i = Math.floor((Math.random() * len));
 	playSound($('.btnSound')[i]["id"]);
+}
+
+function cliquepas(){
+	counter = counter +1;
+	if (counter == 100){
+		location.assign("http://www.pornhub.com/view_video.php?viewkey=1162851739");
+	}
+	
+}
+
+function functdemerde(id){
+	playSound(id);
+	firstQuest(id);
+}
+
+function firstQuest(id){
+	switch(id){
+		case "chie":
+			quete1 = id;
+		break;
+		case "hikku":	
+			if(quete1 == 'chie')
+			{
+				quete1 = "hikku";
+				viewImage('http://img03.deviantart.net/988f/i/2014/018/5/1/lee_sin_pool_party_render_by_argetwing-d72q2i9.png', 200, 200, 'nk logo');
+			}
+		break;
+	}
+}
+
+
+function viewImage(src, width, height, alt) {
+                var img = document.createElement("img");
+				
+				img.src = src;
+				img.width = width;
+				img.height = height;
+				img.alt = alt;
+				
+				document.body.appendChild(img);
 }
 
 $(document).keypress(function(event) {
